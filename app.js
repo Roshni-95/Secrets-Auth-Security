7e434f7397f5eae61009d140a439317990a1fbf0
Roshni <rnp9592@gmail.com>
2021-03-23 23:19:16 -0400

    SecretsL3Security

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const ejs = require("ejs");
 const express = require("express");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
-console.log(process.env.API_KEY);
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -26,7 +26,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ["password"] });
+console.log(md5("qwerty"));
 
 const User = mongoose.model("User", userSchema);
 
@@ -46,7 +46,7 @@ app.post("/register", function(req, res) {
 
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
     });
 
     newUser.save(function(err) {
@@ -60,7 +60,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser) {
     if (err) {
